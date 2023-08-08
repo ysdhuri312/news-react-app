8d18de134a09090ba7075725cdc09c20a04b9657
Yogesh Dhuri <ysdhuri312@gmail.com>
2023-08-08 23:27:14 +0530

    add styling on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,54 @@ import {
 } from "react-icons/ai";
 
 const Navbar = () => {
+  const navlinks = [
+    "Home",
+    "Top News",
+    "|",
+    "India",
+    "World",
+    "Business",
+    "Technology",
+    "Politics",
+    "Health",
+    "Sport",
+    "Entertainment",
+  ];
+
   return (
     <>
-      <div className=" primary-nav tracking-wide flex justify-between items-center px-28">
-        <div className="text-2xl p-4 font-semibold ">News9</div>
-        <div className="search">
-          <div className="absolute">
+      <div
+        id="primary-nav"
+        className="tracking-wide flex justify-between items-center px-[7%] py-[1.5rem]"
+      >
+        <h1 className="text-2xl opacity-90 uppercase font-extrabold font-cursive">
+          News24
+        </h1>
+        <div id="search" className="relative">
+          <div className="absolute text-2xl translate-x-2/4 translate-y-2/4">
             <AiOutlineSearch />
           </div>
-          <input type="text" placeholder="Search" />
+          <input
+            className="xl:px-48 py-2 rounded-full text-2xl text-center tracking-wider shadow-md focus:outline-none opacity-70 lg:px-25"
+            type="text"
+            placeholder="Search"
+          />
         </div>
-        <div className="flex gap-3">
+        <div className="flex gap-4 text-3xl opacity-60">
           <AiOutlineQuestionCircle />
           <AiOutlineSetting />
         </div>
       </div>
-      <div clasName="secondary-nav"></div>
+      <div
+        id="secondary-nav"
+        className="flex justify-between items-center px-[7%] border-b-2 py-2 mb-2 rounded-lg"
+      >
+        {navlinks.map((navlink, index) => (
+          <ul key={index}>
+            <li className="opacity-70">{navlink}</li>
+          </ul>
+        ))}
+      </div>
     </>
   );
 };
